c66a3e60ef893894c4fb9d7294e8fa2bb353cde9
anhntl <lamanh.090423@gmail.com>
2025-02-08 16:13:45 +0700

    initail Second assignment commit

---
+++ Second-assignment/test2.php
@@ -0,0 +1,21 @@
+<?php
+$x = "Em iu anh PLT ";
+
+var_dump($x) ;
+
+$y = 1000;
+var_dump($y);
+
+$z = 3.14;
+var_dump($z);
+
+$g = true;
+var_dump($g);
+
+$myName = "Lam Anh";
+echo "Tên tôi là $myName";
+$myNameNumber = strlen($myName);
+echo "Tên tôi bao gồm $myNameNumber kí tự";
+$myNameUpper = strtoupper($myName);
+echo "Tên tôi sau khi viết hoa là : $myNameUpper";
+?>
