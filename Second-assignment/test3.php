4e9f2af0adbbedf6174023f78947658b37f90451
anhntl <lamanh.090423@gmail.com>
2025-02-09 20:47:27 +0700

    update test 3

---
+++ Second-assignment/test3.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Chong";
+$character = "dep trai vip pro nhieu tien";
+$fulltring = $name . " " . $character;
+echo $fulltring;
+echo substr($fulltring, 6, 3);
+echo " Tôi bị \"hâm\"";
+?>
