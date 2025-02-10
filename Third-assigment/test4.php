3d0971ecdeb27a1df594cfb7e3a35bc49f6e437f
Anh Nguyen Thi Lam (JDC) <anh.nguyen78@ntq-solution.com.vn>
2025-02-10 17:38:06 +0700

    update test 4 ver 2

--- Third-assigment/test4.php
+++ Third-assigment/test4.php
@@ -1,3 +1,30 @@
 <?php 
-$x = "abc";
+$numberA = 10.75;
+$numberA_inter = (int)$numberA;
+echo $numberA_inter;
+
+echo "<br>";
+
+$result = sqrt($numberA_inter);
+echo $result;
+
+echo "<br>";
+
+echo round($result,0);
+
+echo "<br>";
+
+$numberB = rand(1,20);
+
+echo "So A là :" . $numberA_inter;
+echo "<br>";
+echo "So B là : " . $numberB;
+echo "<br>";
+echo "Ket qua A - B la :" . ($numberA_inter - $numberB);
+echo "<br>";
+echo "Ket qua A + B la :" . ($numberA_inter + $numberB);
+echo "<br>";
+echo "Ket qua A * B la :" . ($numberA_inter * $numberB);
+echo "<br>";
+echo "Ket qua A / B la :" . ($numberA_inter / $numberB);
 ?>
