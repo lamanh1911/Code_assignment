fb9819e42295873695c9eae2f667a07316490be8
Vk Ec EC <lamanh.090423@gmai;.com>
2025-02-27 15:37:12 +0700

    update code form sum up

--- Form-assignment/form_handling.php
+++ Form-assignment/form_handling.php
@@ -1,14 +1,16 @@
 <?php
-function sumUP(){
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-$num1 = $_POST["num1"];
-$num2 = $_POST["num2"];
-if (is_numeric($num1) && is_numeric($num2)){
-    $sum = $num1 + $num2;
-    echo "So thu nhat la: $num1 <br>";
-    echo "So thu nhat la: $num2 <br>";
-    echo "Gia tri tong la: $sum";
-}
-}};
+function sumUP()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+
+        if (is_numeric($num1) && is_numeric($num2)) {
+            $sum = $num1 + $num2;
+            echo "So thu nhat la: $num1 <br>";
+            echo "So thu nhat la: $num2 <br>";
+            echo "Gia tri tong la: $sum";
+        }
+    }
+};
 sumUP();
-?>
